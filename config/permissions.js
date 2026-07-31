7dc75a676df00743b17fe889f8e1f38da596d506
leopoissonnier707-png <leopoissonnier707@gmail.com>
2026-07-31 15:10:59 +0200

    Create permissions.js

---
+++ config/permissions.js
@@ -0,0 +1,10 @@
+module.exports = {
+  PERMISSIONS: {
+    ACCESS_PANEL: 'access_panel',
+    MANAGE_ORDERS: 'manage_orders',      // Accepter / gérer les commandes
+    ADD_CATALOG: 'add_catalog',          // Ajouter des items
+    DELETE_CATALOG: 'delete_catalog',    // Supprimer des items
+    MANAGE_ROLES: 'manage_roles',        // Configurer les permissions des rôles
+    VIEW_USER_LOGS: 'view_user_logs'     // Rechercher un utilisateur et voir ses logs
+  }
+};
